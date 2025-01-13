9d829804656b250d8960794b6c11f92cf8c0802a
Testuser-at-914700456451 <adityatalvadekar@orientindia.net>
2025-01-13 15:30:34 +0530

    Added some code

---
+++ ui/react-jsx.ts
@@ -0,0 +1,11 @@
+declare module 'react/jsx-runtime' {
+    export namespace JSX {
+      interface Element {}
+      interface IntrinsicElements {
+        [elemName: string]: any;
+      }
+    }
+    export const jsx: any;
+    export const jsxs: any;
+    export const Fragment: any;
+  }
